4631fcb119d86619270648d24389b5c4bce8cbc6
Ali Abdul-Karim <ali.abdulkarim90@gmail.com>
2013-11-06 21:11:53 +0200

    Added project files.

---
+++ dogeDash/dogeDash-desktop/src/com/alicode/game/dogedash/Main.java
@@ -0,0 +1,16 @@
+package com.alicode.game.dogedash;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "dogeDash";
+		cfg.useGL20 = true;
+		cfg.width = 800;
+		cfg.height = 480;
+		
+		new LwjglApplication(new DogeDashCore(), cfg);
+	}
+}
